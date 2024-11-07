268c4582a37d38f972ca03afae4ed755a0c39fa3
nicol <nicol@PC_NICO>
2024-11-07 11:09:18 +0100

    Clase Mensaje con el atributo Cabecera

--- src/resources/Mensaje.java
+++ src/resources/Mensaje.java
@@ -5,18 +5,38 @@ import org.w3c.dom.Document;
 public class Mensaje {
 
 	    private Document contenido;
+	    private String cabecera;
 
+	    
+	    //CONSTRUCTORES
+	    public Mensaje() {
+	    }
+	    
 	    public Mensaje(Document contenido) {
 	        this.contenido = contenido;
 	    }
 
-	    // Getter para obtener el contenido del mensaje
+	    public Mensaje(String cabecera, Document contenido) {
+	    	this.cabecera = cabecera;
+	    	this.contenido = contenido;
+	    }
+	    
+	    //GETTERS Y SETTERS
 	    public Document getContenido() {
 	        return contenido;
 	    }
 
-	    // Setter para actualizar el contenido del mensaje
 	    public void setContenido(Document contenido) {
 	        this.contenido = contenido;
 	    }
+
+		public String getCabecera() {
+			return cabecera;
+		}
+
+		public void setCabecera(String cabecera) {
+			this.cabecera = cabecera;
+		}
+	    
+	    
 	}
