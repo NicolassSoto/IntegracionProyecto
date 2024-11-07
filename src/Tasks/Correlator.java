bdaa85a371ac37adabb60a5b73e24b613321f0e5
nicol <nicol@PC_NICO>
2024-11-07 12:54:21 +0100

    Correlator y Distributor programados

--- src/Tasks/Correlator.java
+++ src/Tasks/Correlator.java
@@ -1,10 +1,55 @@
 package Tasks;
 
+import java.util.Iterator;
+import java.util.List;
+
+import conexion.Slot;
+import resources.Mensaje;
 
 //Correlaciona los mensajes de sus dos entradas (Normalmente usando el id) Y los saca al mismo tiempo por sus dos salidas.
 public class Correlator implements ITask{
 
+	private List<Slot> entrada;
+	private List<Slot> salida;
+	
+	private List<Mensaje> A, B;
+	
 public void run() {
 		
+		while(!entrada.get(0).isEmpty()) {
+			A.add(entrada.get(0).extraerMensaje());
+		}
+	
+		while(!entrada.get(1).isEmpty()) {
+			B.add(entrada.get(1).extraerMensaje());
+		}
+	
+		//Esta forma de buscar los mensajes coincidentes solo funcionaria en secuencial, ya que damos por hecho que
+		//cada mensaje tiene su coincidente. En paralelo esto no sería así, ya que puede no haber llegado aun.
+		
+		 Iterator<Mensaje> iteratorA = A.iterator();
+		
+		  while (iteratorA.hasNext()) {
+		        Mensaje mensajeA = iteratorA.next();
+		        Iterator<Mensaje> iteratorB = B.iterator();
+
+		        while (iteratorB.hasNext()) {
+		            Mensaje mensajeB = iteratorB.next();
+
+		            if (mensajeA.getCabecera().equals(mensajeB.getCabecera())) {
+		                // Si encuentra coincidencia los envia
+		                salida.get(0).añadirABuffer(mensajeA);
+		                salida.get(1).añadirABuffer(mensajeB);
+
+		                //Los borra de las listas y pasa al siguiente Mensaje
+		                iteratorA.remove();
+		                iteratorB.remove();
+		                break;
+		            }
+		        }
+		    }
+	
+	
+	
 	}
 }
--- src/Tasks/Distributor.java
+++ src/Tasks/Distributor.java
@@ -1,10 +1,83 @@
 package Tasks;
 
+import java.util.List;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import conexion.Slot;
+import resources.Mensaje;
 
 //Distribuye los mensajes de una entrada hacia varias salidas siguiendo un criterio.
 public class Distributor implements ITask{
 
+	private List<Slot> entrada;
+	private List<Slot> salida;
+	
+	public Distributor() {}
+	
+	public Distributor(List<Slot> entrada, List<Slot> salida) {
+		this.entrada = entrada;
+		this.salida = salida;	
+	}
+	
+	public void setEntrada(List<Slot> entrada) {
+		this.entrada = entrada;
+	}
+	
+	public void setSalida(List<Slot> salida) {
+		this.salida = salida;
+	}
+	
+	
 public void run() {
 		
+	//Se crea una instancia XPath y se define la expresion
+	//Por ejemplo vamos a obtener en un String el atributo tipo
+	
+	
+	XPath xpath = XPathFactory.newInstance().newXPath();
+	
+	try {
+		XPathExpression expr = xpath.compile("//tipo/text()");
+	
+	
+	
+	//Entramos en el bucle para comprobar cada mensaje y enviarlo a una salida concreta segun el tipo
+	while(!entrada.get(0).isEmpty()) {
+		
+		Mensaje m = entrada.get(0).extraerMensaje();
+		
+		String tipo = (String) expr.evaluate(m.getContenido(), XPathConstants.STRING);
+		
+		
+		
+		switch(tipo) {
+		
+		case "tipo1": 
+			salida.get(0).añadirABuffer(m);
+			break;
+			
+			
+		case "tipo2":
+			salida.get(1).añadirABuffer(m);
+			break;
+		default:
+		
+		}
+		
+		
+	}
+	
+	
+	} catch (XPathExpressionException e) {
+		e.printStackTrace();
+	}
+	
+	
+	
 	}
 }
