31cc481433f0f5c109c73c07e3ad82f0ce2cc724
nicol <nicol@PC_NICO>
2024-11-18 09:09:51 +0100

    Clase Mensaje definitiva

--- src/resources/Mensaje.java
+++ src/resources/Mensaje.java
@@ -4,53 +4,66 @@ import org.w3c.dom.Document;
 
 public class Mensaje {
 
+		//CABECERA
+		private String idMensaje;
+		private String idConjunto;
+		int nMensajesEnConjunto;
+	
+		//CUERPO
 	    private Document contenido;
-	    private String cabecera;
-	    private int idTrozo;
 
 	    
-	    public Mensaje(Document contenido, String cabecera, int idTrozo) {
-			super();
-			this.contenido = contenido;
-			this.cabecera = cabecera;
-			this.idTrozo = idTrozo;
-		}
-
-		public int getnMensaje() {
-			return idTrozo;
-		}
-
-		public void setnMensaje(int nMensaje) {
-			this.idTrozo = nMensaje;
-		}
-
-		//CONSTRUCTORES
 	    public Mensaje() {
+	    	
 	    }
 	    
-	    public Mensaje(Document contenido) {
-	        this.contenido = contenido;
-	    }
-
-	    public Mensaje(String cabecera, Document contenido) {
-	    	this.cabecera = cabecera;
-	    	this.contenido = contenido;
-	    }
 	    
-	    //GETTERS Y SETTERS
-	    public Document getContenido() {
-	        return contenido;
-	    }
-
-	    public void setContenido(Document contenido) {
-	        this.contenido = contenido;
-	    }
-
-		public String getCabecera() {
-			return cabecera;
+	    
+		public Mensaje(String idMensaje, String idConjunto, int nMensajesEnConjunto, Document contenido) {
+			super();
+			this.idMensaje = idMensaje;
+			this.idConjunto = idConjunto;
+			this.nMensajesEnConjunto = nMensajesEnConjunto;
+			this.contenido = contenido;
 		}
 
-		public void setCabecera(String cabecera) {
-			this.cabecera = cabecera;
-		} 
+		public String getIdMensaje() {
+			return idMensaje;
+		}
+
+		public void setIdMensaje(String idMensaje) {
+			this.idMensaje = idMensaje;
+		}
+
+		public String getIdConjunto() {
+			return idConjunto;
+		}
+
+		public void setIdConjunto(String idConjunto) {
+			this.idConjunto = idConjunto;
+		}
+
+		public int getnMensajesEnConjunto() {
+			return nMensajesEnConjunto;
+		}
+
+		public void setnMensajesEnConjunto(int nMensajesEnConjunto) {
+			this.nMensajesEnConjunto = nMensajesEnConjunto;
+		}
+
+		public Document getContenido() {
+			return contenido;
+		}
+
+		public void setContenido(Document contenido) {
+			this.contenido = contenido;
+		}
+	    
+
+	    
+	    
+	   
+	 
+	    
+	    
 	}
