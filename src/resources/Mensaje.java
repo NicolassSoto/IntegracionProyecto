9c184ab97a8b777889bd7fe88f0ae96876c7d4bc
raulp <raulp@Ral>
2024-11-25 18:10:56 +0100

    Mensaje con id unico

--- src/resources/Mensaje.java
+++ src/resources/Mensaje.java
@@ -5,8 +5,9 @@ import org.w3c.dom.Document;
 public class Mensaje {
 
     //CABECERA
-	private String idMensaje;
-	private String idMensajeCorrelacion;
+    private String idMensaje;
+    private static int contador = 0;
+    private String idMensajeCorrelacion;
     private String idConjunto;
     private int posicionEnConjunto;
     private Document original;
@@ -21,37 +22,37 @@ public class Mensaje {
 
     public Mensaje(String idMensaje, String idConjunto, int nMensajesEnConjunto, Document contenido) {
         super();
-        this.idMensaje = idMensaje;
+        this.idMensaje = String.valueOf(++contador);
         this.idConjunto = idConjunto;
         this.nMensajesEnConjunto = nMensajesEnConjunto;
         this.contenido = contenido;
     }
 
     public String getIdMensajeCorrelacion() {
-		return idMensajeCorrelacion;
-	}
+        return idMensajeCorrelacion;
+    }
 
-	public void setIdMensajeCorrelacion(String idMensajeCorrelacion) {
-		this.idMensajeCorrelacion = idMensajeCorrelacion;
-	}
+    public void setIdMensajeCorrelacion(String idMensajeCorrelacion) {
+        this.idMensajeCorrelacion = idMensajeCorrelacion;
+    }
 
-	public int getPosicionEnConjunto() {
-		return posicionEnConjunto;
-	}
+    public int getPosicionEnConjunto() {
+        return posicionEnConjunto;
+    }
 
-	public void setPosicionEnConjunto(int posicionEnConjunto) {
-		this.posicionEnConjunto = posicionEnConjunto;
-	}
+    public void setPosicionEnConjunto(int posicionEnConjunto) {
+        this.posicionEnConjunto = posicionEnConjunto;
+    }
 
-	public Document getOriginal() {
-		return original;
-	}
+    public Document getOriginal() {
+        return original;
+    }
 
-	public void setOriginal(Document original) {
-		this.original = original;
-	}
+    public void setOriginal(Document original) {
+        this.original = original;
+    }
 
-	public String getIdMensaje() {
+    public String getIdMensaje() {
         return idMensaje;
     }
 
