d532ba2ebb8a304397c241ddedf87db3243cff3e
raulp <raulp@Ral>
2024-11-26 14:02:32 +0100

    Agregar puertos y conectores al main

--- src/Inicio/main.java
+++ src/Inicio/main.java
@@ -22,63 +22,64 @@ import java.io.StringWriter;
 
 public class main {
 //creo que ya esta arreglado
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerException {
 
-		String xml = "<cafe_order><order_id>2</order_id><drinks><drink><name>cafe</name><type>hot</type></drink><drink><name>chocolate</name><type>hot</type></drink><drink><name>coca-cola</name><type>cold</type></drink></drinks></cafe_order>";
-		
-		String xml2= "<cafe_order><order_id>6</order_id><drinks><drink><name>te</name><type>hot</type></drink><drink><name>tila</name><type>hot</type></drink><drink><name>coca-cola</name><type>cold</type></drink><drink><name>guarana</name><type>cold</type></drink><drink><name>cerveza</name><type>cold</type></drink></drinks></cafe_order>";
-		
-		Slot entrada = new Slot();
-		Slot salida = new Slot();
-		
-		Slot salidahot = new Slot();
-		Slot salidacold = new Slot();
-		
-		List<Slot> salidas = new ArrayList<>();
-		
-		salidas.add(salidahot);
-		salidas.add(salidacold);
-		
-		Splitter spl = new Splitter(entrada, salida);
-		
-		Mensaje m = new Mensaje();
-		
-		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-         DocumentBuilder builder = factory.newDocumentBuilder();
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerException {
 
-         // Convertir el XML en un flujo de entrada y analizarlo en un Document
-         Document inputDoc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+        String xml = "<cafe_order><order_id>2</order_id><drinks><drink><name>cafe</name><type>hot</type></drink><drink><name>chocolate</name><type>hot</type></drink><drink><name>coca-cola</name><type>cold</type></drink></drinks></cafe_order>";
 
-         m.setContenido(inputDoc);
-         entrada.setMensaje(m);  
-         Mensaje m2 = new Mensaje();
-         Document inputDoc2 = builder.parse(new ByteArrayInputStream(xml2.getBytes()));
-         m2.setContenido(inputDoc2);
-         entrada.setMensaje(m2);
-         
-         spl.run();
-         
-         List<Mensaje> men = salida.getListaMensajes();
-         
-         for(Mensaje mens : men) {
-        	 System.out.println("NMensajes" + mens.getnMensajesEnConjunto() + "   IDCon" + mens.getIdConjunto() + "\n");
-        	 TransformerFactory transformerFactory = TransformerFactory.newInstance();
-             Transformer transformer = transformerFactory.newTransformer();
-             StringWriter writer = new StringWriter();
-             transformer.transform(new DOMSource(mens.getContenido()), new StreamResult(writer));
-             String xmlString = writer.toString();
-             System.out.println(xmlString);
-         }
-         
-	}
-	
-	public static void imprimirDocumento(Document doc) throws Exception {
+        String xml2 = "<cafe_order><order_id>6</order_id><drinks><drink><name>te</name><type>hot</type></drink><drink><name>tila</name><type>hot</type></drink><drink><name>coca-cola</name><type>cold</type></drink><drink><name>guarana</name><type>cold</type></drink><drink><name>cerveza</name><type>cold</type></drink></drinks></cafe_order>";
+
+        Slot entrada = new Slot();
+        Slot salida = new Slot();
+
+        Slot salidahot = new Slot();
+        Slot salidacold = new Slot();
+
+        List<Slot> salidas = new ArrayList<>();
+
+        salidas.add(salidahot);
+        salidas.add(salidacold);
+
+        Splitter spl = new Splitter(entrada, salida);
+
+        Mensaje m = new Mensaje();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Convertir el XML en un flujo de entrada y analizarlo en un Document
+        Document inputDoc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+        m.setContenido(inputDoc);
+        entrada.setMensaje(m);
+        Mensaje m2 = new Mensaje();
+        Document inputDoc2 = builder.parse(new ByteArrayInputStream(xml2.getBytes()));
+        m2.setContenido(inputDoc2);
+        entrada.setMensaje(m2);
+
+        spl.run();
+
+        List<Mensaje> men = salida.getListaMensajes();
+
+        for (Mensaje mens : men) {
+            System.out.println("NMensajes" + mens.getnMensajesEnConjunto() + "   IDCon" + mens.getIdConjunto() + "\n");
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(mens.getContenido()), new StreamResult(writer));
+            String xmlString = writer.toString();
+            System.out.println(xmlString);
+        }
+
+    }
+
+    public static void imprimirDocumento(Document doc) throws Exception {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         DOMSource source = new DOMSource(doc);
         StreamResult result = new StreamResult(System.out);
 
-        transformer.transform(source, result); 
+        transformer.transform(source, result);
     }
 }
--- src/Inicio/mainDefinitivo.java
+++ src/Inicio/mainDefinitivo.java
@@ -1,6 +1,12 @@
 package Inicio;
 
 import Tasks.*;
+import conexion.ConectorDB;
+import conexion.ConectorEntrada;
+import conexion.ConectorSalida;
+import conexion.PuertoEntrada;
+import conexion.PuertoSalida;
+import conexion.PuertoSolicitud;
 import conexion.Slot;
 import resources.Mensaje;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -12,34 +18,33 @@ import java.util.List;
 import resources.XmlTransformer;
 
 public class mainDefinitivo {
-    public static void main(String[] args) throws Exception { 
-        
+
+    public static void main(String[] args) throws Exception {
+
         //Necesarios
-        String file = "ruta/al/archivo.xml"; 
+        String file = "ruta/al/archivo.xml";
         XmlTransformer transformerCold = null;
         XmlTransformer transformerHot = null;
         String tagSplitter = "drink";
         String tagAggregator = "drinks";
-        
+
         List<String> expresiones = new ArrayList<>();
         expresiones.add("//drink[type='cold']"); // Expresión para drinks frías
         expresiones.add("//drink[type='hot']");  // Expresión para drinks calientes
 
-
         //Slots para la comunicación entre tareas
-        
         //Splitter
         Slot splitterInput = new Slot();
         Slot splitterOutput = new Slot();
-        
+
         //Id_Setter
-        Slot salidaIdSetter = new Slot(); 
-        
+        Slot salidaIdSetter = new Slot();
+
         //Distributor 
-        Slot distributorColdInput = new Slot();
-        Slot distributorHotInput = new Slot();
-        List<Slot> salida = List.of(distributorColdInput, distributorHotInput);
-        
+        Slot distributorColdOutput = new Slot();
+        Slot distributorHotOutput = new Slot();
+        List<Slot> salida = List.of(distributorColdOutput, distributorHotOutput);
+
         //Replicator
         Slot replicatorColdOutputToTranslator = new Slot();
         Slot replicatorHotOutputToTransalator = new Slot();
@@ -47,84 +52,97 @@ public class mainDefinitivo {
         Slot replicatorHotOutputToCorrelator = new Slot();
         List<Slot> replicatorColdOutput = List.of(replicatorColdOutputToTranslator, replicatorColdOutputToCorrelator);
         List<Slot> replicatorHotOutput = List.of(replicatorHotOutputToCorrelator, replicatorHotOutputToTransalator);
-        
+
         //Translator (bebidas frías)
         Slot translatorColdOutput = new Slot();
-        
+
         //Translator (bebidas calientes)
         Slot translatorHotOutput = new Slot();
-        
+
         //Correlator (bebidas frías)
         Slot correlatorColdInput1 = new Slot();
         Slot correlatorColdInput2 = new Slot();
-        List <Slot> entradaCorrelatorCold = List.of(correlatorColdInput1, correlatorColdInput2);
+        List<Slot> entradaCorrelatorCold = List.of(correlatorColdInput1, correlatorColdInput2);
         Slot correlatorColdOutput1 = new Slot();
         Slot correlatorColdOutput2 = new Slot();
-        List <Slot> correlatorColdOutput = List.of(correlatorColdOutput1, correlatorColdOutput2);
-        
+        List<Slot> correlatorColdOutput = List.of(correlatorColdOutput1, correlatorColdOutput2);
+
         //Correlator (bebidas calientes)
+        Slot correlatorHotInput1 = new Slot();
+        Slot correlatorHotInput2 = new Slot();
+        List<Slot> entradaCorrelatorHot = List.of(correlatorHotInput1, correlatorHotInput2);
         Slot correlatorHotOutput1 = new Slot();
         Slot correlatorHotOutput2 = new Slot();
-        List <Slot> correlatorHotInput = List.of(correlatorHotOutput1, correlatorHotOutput2);
-        
+        List<Slot> correlatorHotInput = List.of(correlatorHotOutput1, correlatorHotOutput2);
+
         //Context enricher (bebidas frías)
         Slot enricherColdContext = new Slot();
         Slot enricherColdMessaje = new Slot();
-        List<Slot> enricherColdOutput = List.of(enricherColdContext, enricherColdMessaje); 
-        
-         //Context enricher (bebidas calientes)
+        List<Slot> enricherColdOutput = List.of(enricherColdContext, enricherColdMessaje);
+
+        //Context enricher (bebidas calientes)
         Slot enricherHotContext = new Slot();
         Slot enricherHotMessaje = new Slot();
-        List<Slot> enricherHotOutput = List.of(enricherHotContext, enricherHotMessaje);  
-        
-        
+        List<Slot> enricherHotOutput = List.of(enricherHotContext, enricherHotMessaje);
+
         //Merger
         List<List<Slot>> mergerInput = List.of(enricherColdOutput, enricherHotOutput);
         Slot mergerOutput = new Slot();
-        
+
         //Aggregator
         Slot aggregatorOutput = new Slot();
-        
 
+        //PUERTOS
+        PuertoEntrada pEntrada = new PuertoEntrada(splitterInput);
+        PuertoSolicitud pSolHot = new PuertoSolicitud(translatorHotOutput, correlatorHotInput2);
+        PuertoSolicitud pSolCold = new PuertoSolicitud(translatorColdOutput, correlatorColdInput2);
+        PuertoSalida pSalida = new PuertoSalida(aggregatorOutput);
+        
+        //CONECTORES
+        ConectorEntrada conectorEntrada = new ConectorEntrada(pEntrada, file);
+        ConectorDB conectorHot = new ConectorDB(pSolHot);
+        ConectorDB conectorCold = new ConectorDB(pSolCold);
+        ConectorSalida conectorSalida = new ConectorSalida(pSalida, file);
+        
         //CONEXIÓN ENTRE TAREAS
         Splitter splitter = new Splitter(splitterInput, splitterOutput, tagSplitter);
-        
+
         CorrelationIdSetter idSetter = new CorrelationIdSetter(splitterOutput, salidaIdSetter);
-        
+
         Distributor distributor = new Distributor(salidaIdSetter, salida, expresiones);
-        
-        Replicator replicatorCold = new Replicator(distributorColdInput, replicatorColdOutput);    
-        Replicator replicatorHot = new Replicator(distributorHotInput, replicatorHotOutput);
-        
+
+        Replicator replicatorCold = new Replicator(distributorColdOutput, replicatorColdOutput);
+        Replicator replicatorHot = new Replicator(distributorHotOutput, replicatorHotOutput);
+
         Translator translatorCold = new Translator("", replicatorColdOutputToTranslator, translatorColdOutput);
         Translator translatorHot = new Translator("", replicatorHotOutputToTransalator, translatorHotOutput);
-        
+
         Correlator correlatorCold = new Correlator(replicatorColdOutput, correlatorColdOutput);
         Correlator correlatorHot = new Correlator(replicatorHotOutput, correlatorHotInput);
-       
+
         ContextEnricher enricherCold = new ContextEnricher(correlatorColdOutput, enricherColdContext, salidaIdSetter, transformerCold);
         ContextEnricher enricherHot = new ContextEnricher(correlatorColdOutput, enricherColdContext, salidaIdSetter, transformerHot);
-        
+
         Merger merger = new Merger(mergerInput, mergerOutput);
-        
+
         Aggregator aggregator = new Aggregator(mergerOutput, aggregatorOutput, tagAggregator);
 
         Mensaje mensaje = crearMensajeDesdeXML(file);
         splitterInput.setMensaje(mensaje);
 
         splitter.run();
-        idSetter.run(); 
-        distributor.run(); 
-        replicatorCold.run();  
-        replicatorHot.run();  
-        translatorCold.run(); 
-        translatorHot.run();  
-        correlatorCold.run();  
+        idSetter.run();
+        distributor.run();
+        replicatorCold.run();
+        replicatorHot.run();
+        translatorCold.run();
+        translatorHot.run();
+        correlatorCold.run();
         correlatorHot.run();
-        enricherCold.run(); 
+        enricherCold.run();
         enricherHot.run();
-        merger.run();  
-        aggregator.run();  
+        merger.run();
+        aggregator.run();
 
         List<Mensaje> resultadoFinal = aggregatorOutput.getListaMensajes();
         for (Mensaje m : resultadoFinal) {
@@ -142,6 +160,3 @@ public class mainDefinitivo {
         return mensaje;
     }
 }
-
-
-
